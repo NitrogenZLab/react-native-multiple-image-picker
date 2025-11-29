4eabae1c57c2b2681f2558097685ab8ab75ac289
song <>
2025-11-29 19:56:08 +0700

    fix 让 @baronha/react-native-multiple-image-picker 在新架构下正常跑通

--- react-native.config.js
+++ react-native.config.js
@@ -1,16 +1,17 @@
 // https://github.com/react-native-community/cli/blob/main/docs/dependencies.md
 
+const path = require('path')
+
 module.exports = {
   dependency: {
     platforms: {
-      /**
-       * @type {import('@react-native-community/cli-types').IOSDependencyParams}
-       */
-      ios: {},
-      /**
-       * @type {import('@react-native-community/cli-types').AndroidDependencyParams}
-       */
-      android: {},
+      ios: {
+        // Help autolinking find the podspec when using file:../ or symlinked installs.
+        podspecPath: path.join(__dirname, 'MultipleImagePicker.podspec'),
+      },
+      android: {
+        sourceDir: path.join(__dirname, 'android'),
+      },
     },
   },
 }
